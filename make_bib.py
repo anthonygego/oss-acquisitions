1fc99a837d06b41a57ffdf620779acfa50346c72
Anthony Gégo <anthonygego@hotmail.com>
2021-06-22 15:18:01 +0200

    add processing cache

--- make_bib.py
+++ make_bib.py
@@ -1,12 +1,25 @@
+import os
 import csv
+import json
+import argparse
 import requests
 
 # URL To the Zotero translation server
 ZTS_URL = "http://127.0.0.1:1969"
 
+# Parse arguments
+parser = argparse.ArgumentParser()
+parser.add_argument("-u", "--update-cache", help="increase output verbosity", action="store_true")
+args = parser.parse_args()
+
+# Load cache if exists
+biblio = {}
+if not args.update_cache and os.path.exists("cache.json"):
+    print("Load existing cache")
+    with open("cache.json") as jsonfile:
+        biblio = json.load(jsonfile)
 
 # Process entries line per line
-biblio = []
 with open("list.csv") as csvfile:
     row_reader = csv.reader(csvfile)
     for idx, row in enumerate(row_reader):
@@ -14,30 +27,37 @@ with open("list.csv") as csvfile:
         if idx == 0:
             continue
 
-        # Fetch URL
+        # Process URL
         print("Processing line {}".format(idx))
         url = row[7]
+        if url in biblio:
+            # Use cache
+            print("Line already in cache")
+            bib_entry = biblio[url]
+        else:
+            # Request server
+            try:
+                r = requests.post(ZTS_URL + "/web", data=url, headers={'Content-type': 'text/plain'})
+                bib_entry = r.json()[0]
+            except Exception as e:
+                print("ERROR at line {}, url[{}]: {}".format(str(idx), url, str(e)))
+                continue
 
-        # Request server
-        r = requests.post(ZTS_URL + "/web", data=url, headers={'Content-type': 'text/plain'})
+        # Remove abstract if exists
+        bib_entry.pop("abstractNote", None)
 
-        try:
-            bib_entry = r.json()[0]
+        # Set the citation key
+        bib_entry["citationKey"] = "oss_acquisition_" + str(idx)
 
-            # Remove abstract if exists
-            bib_entry.pop("abstractNote", None)
+        # Add entry to bibliography
+        biblio[url] = bib_entry
 
-            # Set the citation key
-            bib_entry["citationKey"] = "oss_acquisition_" + str(idx)
-
-            # Add entry to bibliography
-            biblio.append(bib_entry)
-        except Exception as e:
-            print("ERROR at line {}, url[{}]: {}".format(str(idx), url, str(e)))
-
+# Write cache to file
+with open("cache.json", "w") as jsonfile:
+    json.dump(biblio, jsonfile, indent=4)
 
 # Export to bibtex format
-r = requests.post(ZTS_URL + "/export?format=bibtex", json=biblio)
+r = requests.post(ZTS_URL + "/export?format=bibtex", json=list(biblio.values()))
 
 # Write result to file
 with open('output.bib', 'w') as f:
